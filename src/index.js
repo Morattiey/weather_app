56041d83af871b4cc8000ef600e0670dbbfeb3f3
Morattiey <moratimolapisi2@gmail.com>
2023-06-02 17:55:30 +0200

    updated the app.js file

--- src/index.js
+++
@@ -1,4 +0,0 @@
-let apiKey = "1fa372bd37320d241516f3f0ef11646c";
-let city = "Johannesburg";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Johannesburg&appid={API key}&units=metric`;
-console.log(apiUrl);
